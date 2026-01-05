209dbea91b4d8d18c1fd847159765a8afc311fe7
RafaelNCST <rafaelnccolares@gmail.com>
2026-01-05 15:39:40 -0400

    refactor: remove unused components and update settings
    - Deleted several unused components including LinkTooltip, RichTextEditor, Calendar, Table, Toaster, and various utility functions related to characters and factions.
    - Updated settings.local.json to include a new Bash command for directory removal, enhancing the configuration options.
    
    This cleanup improves the codebase by removing obsolete files and optimizing the settings for better usability.

--- src/pages/dashboard/tabs/factions/utils/formatters/get-title-name.ts
+++
@@ -1,6 +0,0 @@
-import { IFaction } from "@/types/faction-types";
-
-export function getTitleName(titleId: string, faction: IFaction): string {
-  const title = faction.titles.find((t) => t.id === titleId);
-  return title?.name || "Membro";
-}
